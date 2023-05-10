dc0e28b13b037d076f183ae46807df04d2d3cb17
Felipe Gueller <felipe.gueller@gmail.com>
2023-05-10 12:27:17 -0300

    :tada: feat: creating the app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainSidebarModule } from './main-sidebar/main-sidebar.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MainSidebarModule,
     AppRoutingModule
   ],
   providers: [],
