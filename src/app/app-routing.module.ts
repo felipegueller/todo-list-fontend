8d3ab7bdd3598726f100beae963bf3eddd21937a
Felipe Gueller <felipe.gueller@gmail.com>
2023-05-10 23:27:29 -0300

    :sparkles: feat: setting the component routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'task-list',
+    loadChildren: () =>
+      import('./task-list/task-list.module').then((m) => m.TaskListModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'task-list'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
