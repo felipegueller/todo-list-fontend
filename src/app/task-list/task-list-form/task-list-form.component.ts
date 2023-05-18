982fd731a4acd23ab1c141436836096e70920021
Felipe Gueller <felipe.gueller@gmail.com>
2023-05-18 20:12:31 -0300

    :wrench: fix: adding the task-list form-component

---
+++ src/app/task-list/task-list-form/task-list-form.component.ts
@@ -0,0 +1,93 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+
+import {
+  applyValidationClasses,
+  getFormControl,
+  verifyFormValidations,
+  resetForm,
+} from 'src/helpers/form-validation-functions';
+
+import { TaskList } from './../../../interfaces/task-list.interface';
+
+import 'jquery';
+declare var $: any;
+
+@Component({
+  selector: 'app-task-list-form',
+  templateUrl: './task-list-form.component.html',
+  styleUrls: ['./task-list-form.component.css'],
+})
+export class TaskListFormComponent implements OnInit, OnChanges {
+  @Input() dataToUpdate!: TaskList;
+  @Output() firewallAdd: EventEmitter<TaskList> = new EventEmitter();
+  @Output() firewallUpdate: EventEmitter<TaskList> = new EventEmitter();
+
+  reactiveForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.reactiveForm = this.formBuilder.group({
+      name: [null, [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    resetForm(this.reactiveForm);
+
+    this.dataToUpdate = changes?.['dataToUpdate']?.currentValue;
+    if (this.dataToUpdate) this.setUpdateDataOnForm();
+  }
+
+  // Class methods
+
+  getControl(control: string) {
+    return getFormControl(this.reactiveForm, control);
+  }
+
+  applyInputValidationClasses(control: string) {
+    const field = this.getControl(control);
+    return field ? applyValidationClasses(field) : null;
+  }
+
+  closeModal(): void {
+    $('#firewallModal').modal('hide');
+  }
+
+  emitData(): void {
+    const data: TaskList = { ...this.reactiveForm.value };
+    if (this.dataToUpdate) data['id'] = this.dataToUpdate.id
+
+    this.dataToUpdate
+      ? this.firewallUpdate.emit(data)
+      : this.firewallAdd.emit(data);
+
+    resetForm(this.reactiveForm);
+    this.closeModal();
+  }
+
+  onSubmit() {
+    this.reactiveForm.valid
+      ? this.emitData()
+      : verifyFormValidations(this.reactiveForm);
+  }
+
+  setUpdateDataOnForm(): void {
+    const { name } = this.dataToUpdate || {};
+
+    this.reactiveForm.patchValue({
+      name: name || ''
+    });
+  }
+
+
+}
